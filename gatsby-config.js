fc7cec5bc00dcaa07de39833c2980173d9c5cb38
Alex Erik <emmx@mail.ru>
2019-10-26 14:27:01 +0300

    added gatsby source wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,19 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+         * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'
+         */
+        baseUrl: `cms.example-wordpress.site`,
+        protocol: `http`,
+        hostingWPCOM: false,
+        useACF: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
